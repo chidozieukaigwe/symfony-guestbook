e776cff70f93e7f7f9c7a011d547618208091f90
Chidozie Ukaigwe <chido@Chidozies-Air.lan>
2025-07-10 06:26:44 +0100

    Add Twig env to and render conferences

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,21 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Twig\Environment;
 
 final class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        return new Response(<<<EOF
-        <html>
-            <body>
-                <h1>Welcome to the Symfony Conference!</h1>
-            </body>
-        </html>
-        EOF);
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
     }
 }
